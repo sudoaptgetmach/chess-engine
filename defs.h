9bc4fbce34fc79c0a6ee0003e977b613c1cdbb44
mach <qluizmachado@gmail.com>
2025-03-28 20:27:53 -0300

    Added defs.h

--- defs.h
+++ defs.h
@@ -10,8 +10,9 @@ typedef unsigned long long U64;
 #define NAME "Chess Engine"
 #define BRD_SQ_NUM 128
 
-enum { EMPTY, wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK };
+#define MAXGAMEMOVES 2048
 
+enum { EMPTY, wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK };
 enum { FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H, FILE_NONE };
 enum { RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8, RANK_NONE };
 
@@ -29,5 +30,42 @@ enum {
  };
 
 enum { FALSE, TRUE };
+
+enum { WKCA = 1, WQCA = 2, BKCA = 4, BQCA = 8 };
+
+typedef struct {
+  int move;
+  int castlePerm;
+  int enPas;
+  int fiftyMove;
+  U64 posKey;
+} S_UNDO;
+
+typedef struct {
+  int pieces[BRD_SQ_NUM];
+  U64 pawns[3];
+
+  int KingSq[2];
+
+  int side;
+  int enPas;
+  int fiftyMove;
+
+  int ply;
+  int hisPly;
+
+  int castlePerm;
+
+  U64 posKey;
+
+  int posNum[13];
+  int bigPce[3];
+  int majPce[3];
+  int minPce[3];
+
+  S_UNDO history[MAXGAMEMOVES];
+} S_BOARD;
+
+#define FR2HQ(f,r) ( (21 + (f) ) + ( (r) * 10) )
 
 #endif //DEFS_H
---
+++ init.c
@@ -0,0 +1,12 @@
+//
+// Created by mach on 28/03/25.
+//
+
+#include "defs.h"
+
+int Sq120To64[BRD_SQ_NUM];
+int Sq64To120[64];
+
+void AllInit() {
+
+}
